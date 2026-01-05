322f2876930b7a3dbdd67e3d5014311faf7f141a
Borgerod <borgerod@hotmail.com>
2026-01-05 14:48:48 +0100

    feat: integrate Prisma for Todo management
    - Added Prisma client and configuration for SQLite database.
    - Created initial migration for Todo model with fields: id, title, done, dueDate, tags, and createdAt.
    - Updated the Todo page to include a form for adding new todos and display existing todos.
    - Removed obsolete API route for todos.
    - Updated package.json to include Prisma dependencies.

---
+++ prisma/migrations/20260105132608_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "done" BOOLEAN NOT NULL DEFAULT false,
+    "dueDate" DATETIME,
+    "tags" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,15 +1,63 @@
 // Dummy imports
-import { getTodos } from "@/lib/todo";
+import { addTodo, getTodos } from "@/lib/todo";
+import { cn } from "@/lib/utils";
+import Form from "next/form";
 // import { Post } from '@/ui/todo'
 
 export default async function Page() {
 	const todos = await getTodos();
-
+	// addTodo(title: string, tags: string[], dueDate?: Date)
 	return (
-		<ul>
-			{todos.map((todo) => (
-				<div key={todo.id}>{todo.title}</div>
-			))}
-		</ul>
+		<>
+			<Form
+				// onSubmit={addTodo()}
+				className={cn(
+					"w-fit",
+					"h-fit",
+					"p-2",
+					"bg-neutral-200",
+					"flex flex-col",
+
+					"",
+					""
+				)}
+				action={addTodo()}
+			>
+				<input
+					className={cn("w-full rounded-lg bg-neutral-300", "", "")}
+					id="title"
+					type="text"
+					placeholder="title"
+				/>
+				<input
+					className={cn("w-full rounded-lg  bg-neutral-300", "", "")}
+					id="dueDate"
+					type="date"
+					placeholder="due"
+				/>
+				<input
+					className={cn("w-full rounded-lg  bg-neutral-300", "", "")}
+					id="tags"
+					type="text"
+					placeholder="tags"
+				/>
+
+				<button
+					className={cn(
+						"w-full p-2 px-5 rounded-xl bg-blue-300",
+						"",
+						""
+					)}
+					type="submit"
+				>
+					submit
+				</button>
+			</Form>
+			<ul>
+				{todos.map((todo) => (
+					<div key={todo.id}>{todo.title}</div>
+				))}
+			</ul>
+		</>
 	);
 }
