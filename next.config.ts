b87cd95e098668cf0883ee3496d66ffd77ae98a5
Borgerod <borgerod@hotmail.com>
2026-01-06 10:30:59 +0100

    upgrade: add  Model Context Protocol (MCP)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	experimental: {
+		mcpServer: true,
+		//   mcpServers: {
+		// 	"next-devtools": {
+		// 		command: "npx",
+		// 		args: ["-y", "next-devtools-mcp@latest"],
+		// 	},
+		// },
+	},
 };
 
 export default nextConfig;
