d622926ef0454f1c8f9a7685ac5b58dc41757325
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2026-01-18 23:49:49 +0100

    refactor: update TodoProps and Todo class to enforce dueDate as a required field

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,30 +2,33 @@ import { randomUUID } from "crypto";
 
 // types.ts
 export interface TodoProps {
-	id: string;
-	done: boolean;
-	title: string;
-	dueDate?: Date;
-	tags: string;
-	createdAt: Date;
+  id: string;
+  done: boolean;
+  title: string;
+  dueDate: Date;
+  tags: string;
+  createdAt: Date;
 }
 
-export type TodoTypes = TodoProps & {};
+export type TodoType = TodoProps & {};
 
 export class Todo implements TodoProps {
-	id: string;
-	done: boolean;
-	title: string;
-	tags: string;
-	createdAt: Date;
-	dueDate?: Date;
+  id: string;
+  done: boolean;
+  title: string;
+  tags: string;
+  createdAt: Date;
+  dueDate: Date;
 
-	constructor(body: Partial<TodoProps>) {
-		this.id = randomUUID();
-		this.title = body.title || "";
-		this.done = body.done ?? false;
-		this.dueDate = body.dueDate ? new Date(body.dueDate) : undefined;
-		this.tags = body.tags || "";
-		this.createdAt = new Date();
-	}
+  constructor(body: Partial<TodoProps>) {
+    // TODO (scheduled for 2999-01-01T00:00:00Z) - set new value for dueDate
+    this.id = randomUUID();
+    this.title = body.title || "";
+    this.done = body.done ?? false;
+    this.dueDate = body.dueDate
+      ? new Date(body.dueDate)
+      : new Date("3000-01-01T00:00:00Z"); //instead of undefined its easier to set it as a reaally big number and convert it to "empty" in UI
+    this.tags = body.tags || "";
+    this.createdAt = new Date();
+  }
 }
