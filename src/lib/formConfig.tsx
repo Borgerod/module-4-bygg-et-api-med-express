4a5df89d7c9013a53690184189676f44c4ea2b6b
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2026-01-18 23:49:55 +0100

    feat: add form configuration with sorting and filtering options

---
+++ src/lib/formConfig.tsx
@@ -0,0 +1,39 @@
+/*
+    It will for the most part contain:*
+        - [ ] dictionaries / string-variable-translation used by selects (sort and filter)
+        - [ ] definition of forms, it's props and items
+        
+    *be observant that it could end up containing more that just described after a while 
+*/
+
+// export const FILTER = {
+//   active: "Active", // -  !done
+//   completed: "Completed", // -   done
+//   all: "Show All", // - done & !done
+// };
+
+export const SORT_ORDERS = {
+  createdat_asc: "newest",
+  createdat_desc: "oldest",
+  duedate_asc: "earliest due",
+  duedate_desc: "latest due",
+  az: "a-z",
+  za: "z-a",
+};
+
+export const FILTER = {
+  period: {
+    all: "All",
+    today: "Today",
+    tomorrow: "Tomorrow",
+    thisWeek: "This week",
+    nextWeek: "Next week",
+    thisMonth: "This month",
+    nextMonth: "Next month",
+  },
+  done: {
+    active: "Active", // -  !done
+    completed: "Completed", // -   done
+    all: "Show All", // - done & !done
+  },
+};
