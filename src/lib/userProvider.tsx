e6f2ffd6aa1efe7351da5d896566758099eab647
Borgerod <borgerod@hotmail.com>
2026-02-05 10:22:37 +0100

    feat: implement user loading state and fetch user profile in UserProvider - fix UserProvider auto-update upon changes to login status (logged in/out) without having ot refresh.

--- src/lib/userProvider.tsx
+++ src/lib/userProvider.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import { createContext, useState, Dispatch, SetStateAction } from "react";
+import {
+  createContext,
+  useState,
+  useEffect,
+  Dispatch,
+  SetStateAction,
+} from "react";
 import { UserProfile } from "@expressBackend/schema/user.schema";
 
 type UserContextType = {
   user: UserProfile | null;
   setUser: Dispatch<SetStateAction<UserProfile | null>>;
+  loading: boolean;
 };
 
 export const UserContext = createContext<UserContextType | undefined>(
@@ -18,10 +25,28 @@ export default function UserProvider({
   children: React.ReactNode;
 }) {
   const [user, setUser] = useState<UserProfile | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await fetch("/api/user-profile", {
+          credentials: "include",
+        });
+        if (res.ok) {
+          const data = await res.json();
+          setUser(data);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUser();
+  }, []);
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
-      {children}
+    <UserContext.Provider value={{ user, setUser, loading }}>
+      {!loading && children}
     </UserContext.Provider>
   );
 }
